bc6b0443f582b96c6daa8bebf545d2b81986eebe
令狐少侠 <czc12580520@gmail.com>
2019-07-25 22:49:39 +0800

    commit

--- src/views/analytics/Analytics.vue
+++ src/views/analytics/Analytics.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="dashboard-analytics">
+  <div>
     <div class="flex flex-wrap w-full">
-      <!-- 卡片 1 -->
+      <!-- 卡片 1：欢迎卡片 -->
       <div class="lg:w-6/12 lg:pr-3 sm:w-full my-3 min-h-full">
         <div class="h-full text-white bg-purple rounded-lg shadow-xl">
           <div class="h-full relative flex flex-col justify-center items-center">
@@ -25,40 +25,95 @@
           </div>
         </div>
       </div>
-      <!-- 卡片 2 -->
+      <!-- 卡片 2：关注者统计卡片 -->
       <div class="lg:w-3/12 lg:pl-3 lg:pr-3  my-3">
         <div class="bg-white rounded-lg shadow-xl h-full">
           <area-chart
             icon="icon-group"
             color="primary"
-            :chartData="subscribersGained"
             type="area"
+            :chartData="subscribersGained"
           ></area-chart>
         </div>
       </div>
-      <!-- 卡片 3 -->
+      <!-- 卡片 3：用户统计卡片 -->
       <div class="lg:w-3/12 lg:pl-3 my-3">
         <div class="bg-white rounded-lg shadow-xl h-full">
           <area-chart
             icon="icon-invite"
             color="warning"
-            :chartData="ordersRecevied"
             type="area"
+            :chartData="ordersRecevied"
           ></area-chart>
         </div>
       </div>
     </div>
     <div class="flex flex-wrap w-full">
       <!-- 卡片 4 -->
-      <div class="lg:w-6/12 lg:pl-3 lg:pr-3 my-3">
-        <div class="bg-white rounded-lg shadow-xl h-full">
-          <div class="w-full lg:w-1/2 xl:w-1/2 flex flex-col lg:mb-0 mb-base">
-            <vue-apex-charts
-              type="bar"
-              height=200
-              :options="salesBar.chartOptions"
-              :series="salesBar.series"
-            />
+      <div class="lg:w-6/12 lg:pr-3 my-3">
+        <div class="p-6 bg-white rounded-lg shadow-xl h-full">
+          <div class="flex">
+            <div class="w-full lg:w-1/2 xl:w-1/2 flex flex-col justify-between pr-4">
+              <div>
+                <div class="mb-1 text-2xl font-bold">2.7K</div>
+                <span class="font-medium text-gray-500">交易额</span>
+                <p class="mt-2 text-xl font-medium">
+                  <span class='text-success'>+5.2%</span> vs last 7 days
+                </p>
+              </div>
+              <vs-button
+                icon="chevron_right"
+                icon-after
+                class="shadow-md w-full lg:mt-0 mt-4"
+              >查看详细</vs-button>
+            </div>
+            <div class="w-full lg:w-1/2 xl:w-1/2 flex flex-col lg:mb-0 mb-base px-2">
+              <vue-apex-charts
+                type="bar"
+                height=200
+                :options="salesBar.chartOptions"
+                :series="salesBar.series"
+              />
+            </div>
+          </div>
+          <vs-divider class="my-6"></vs-divider>
+          <div>
+            <div class="flex">
+              <div class="w-1/2 mb-3 pr-4">
+                <p class="text-gray-600">Goal: $100000</p>
+                <vs-progress
+                  class="block mt-1"
+                  :percent="50"
+                  color="primary"
+                ></vs-progress>
+              </div>
+              <div class="w-1/2 mb-3 pl-4">
+                <p class="text-gray-600">Users: 100K</p>
+                <vs-progress
+                  class="block mt-1"
+                  :percent="60"
+                  color="warning"
+                ></vs-progress>
+              </div>
+            </div>
+            <div class="flex">
+              <div class="w-1/2 mb-3 pr-4">
+                <p class="text-gray-600">Retention: 90%</p>
+                <vs-progress
+                  class="block mt-1"
+                  :percent="70"
+                  color="danger"
+                ></vs-progress>
+              </div>
+              <div class="w-1/2 mb-3 pl-4">
+                <p class="text-gray-600">Duration: 1yr</p>
+                <vs-progress
+                  class="block mt-1"
+                  color="success"
+                  :percent="90"
+                ></vs-progress>
+              </div>
+            </div>
           </div>
         </div>
       </div>
