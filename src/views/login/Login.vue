a859a24c1c68a6a05f2be897212bb28b46bf545c
令狐少侠 <czc12580520@gmail.com>
2019-09-27 23:13:40 +0800

    🏆 新增功能

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -45,13 +45,14 @@
         <vs-tab label="注册">
           <div class="h-full text-base">
             <div class="h-full flex flex-col items-center">
-              <div class="w-4/6 mt-32">
+              <div class="w-4/6 mt-20">
                 <vs-input
                   class="w-full py-2"
                   v-for="(item, i) in signUpInput"
                   :key="i"
                   :type="item.type"
                   :placeholder="item.placeholder"
+                  :description-text="item.description"
                   :warning="item.isWarnng"
                   :warning-text="item.warningText"
                   val-icon-warning="warning"
@@ -84,6 +85,8 @@
 </template>
 
 <script>
+import { register } from '@/request/api/user'
+
 const signInInput = [
   {
     placeholder: '您的账号',
@@ -105,13 +108,15 @@ const signUpInput = [
     placeholder: '昵称',
     value: '',
     type: 'text',
+    description: '不能含特殊符号，如@ / % # &',
     isWarnng: false,
     warningText: '',
   },
   {
-    placeholder: '密码（6-16个字符组成，区分大小写）',
+    placeholder: '密码',
     value: '',
     type: 'password',
+    description: '密码应由6-16个字符组成，区分大小写',
     isWarnng: false,
     warningText: '',
   },
@@ -119,6 +124,7 @@ const signUpInput = [
     placeholder: '手机号码',
     value: '',
     type: 'text',
+    description: '',
     isWarnng: false,
     warningText: '',
   },
@@ -135,7 +141,7 @@ export default {
 
   methods: {
     login() {
-      if (!this.validate()) {
+      if (!this.validate('signIn')) {
         // 非空验证不通过，退出程序
         return
       }
@@ -152,28 +158,72 @@ export default {
       const [username, password] = [this.signInInput[0].value, this.signInInput[1].value]
 
       this.$store
-        .dispatch('user/login', { user_name: username, password })
+        .dispatch('user/login', { username, password })
         .then((code) => {
           if (code === 2000) {
             this.$router.replace('/')
           } else if (code === 3000 || code === 4004) {
             this.showAlert = true
           }
+          // 关闭按钮的加载动画
+          this.$vs.loading.close('#signInBtn > .con-vs-loading')
+          this.signInDisable = false
         })
+    },
 
-      // 关闭按钮的加载动画
-      this.$vs.loading.close('#signInBtn > .con-vs-loading')
-      this.signInDisable = false
+    async register() {
+      if (!this.validate('signUp')) {
+        // 非空验证不通过，退出程序
+        return
+      }
+
+      this.$vs.loading({
+        background: 'primary',
+        color: '#fff',
+        container: '#signUpBtn',
+        scale: 0.45,
+      })
+      this.signUpDisable = true
+
+      try {
+        await register()
+      } catch {
+        console.log('error')
+      } finally {
+        this.$vs.loading.close('#signUpBtn > .con-vs-loading')
+        this.signInDisable = false
+      }
     },
 
     // 输入框非空验证
-    validate() {
-      for (let i = 0; i < 2; i += 1) {
-        if (this.signInInput[i].value.length === 0) {
-          this.signInInput[i].isWarnng = true
-          this.signInInput[i].warningText = (i === 0 ? '请输入账号' : '请输入密码')
-          return false
+    validate(flag) {
+      if (flag === 'signIn') {
+        for (let i = 0; i < 2; i += 1) {
+          if (this.signInInput[i].value.length === 0) {
+            this.signInInput[i].isWarnng = true
+            this.signInInput[i].warningText = (i === 0 ? '请输入账号' : '请输入密码')
+            return false
+          }
         }
+        return true
+      }
+      if (flag === 'signUp') {
+        const text = {
+
+
+          0: '请输入昵称',
+          1: '请输入密码',
+          2: '请输入手机',
+          3: '请输入验证码',
+        }
+        for (let i = 0; i < 4; i += 1) {
+          if (this.signUpInput[i].value.length === 0) {
+            this.signUpInput[i].isWarnng = true
+            this.signUpInput[i].warningText = text[i]
+            return false
+          }
+        }
+        return false
       }
       return true
     },
@@ -184,18 +234,6 @@ export default {
     },
     signUpInputFocus(i) {
       this.signUpInput[i].isWarnng = false
-    },
-
-    register() {
-      this.$vs.loading({
-        background: 'primary',
-        color: '#fff',
-        container: '#signUpBtn',
-        scale: 0.45,
-      })
-      this.signUpDisable = true
-      this.$vs.loading.close('#signUpBtn > .con-vs-loading')
-      this.signInDisable = false
     },
   },
 }
@@ -210,6 +248,10 @@ export default {
   min-width: 1000px;
   height: 562px;
   background-image: url("~@/assets/images/pages/login/login_bg.png");
+}
+
+.vs-tabs-primary button:not(:disabled):hover {
+  color: #fff !important; // 修正样式
 }
 
 // 重置tab选项卡的高度
