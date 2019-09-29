083707ad23332b8327232bd6e1fe472ec7b4ffc2
令狐少侠 <czc12580520@gmail.com>
2019-09-29 22:29:43 +0800

    ⚠️ 添加和删除 ESLint 规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,15 +15,11 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-param-reassign': ['error', { props: false }],
     'no-shadow': ['error', { allow: ['state'] }],
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
     semi: ['error', 'never'],
   },
 
   parserOptions: {
     parser: 'babel-eslint',
   },
-
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
 }
