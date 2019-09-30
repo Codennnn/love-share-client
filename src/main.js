fa349777d263be51e1150f885b43aa4913debeac
令狐少侠 <czc12580520@gmail.com>
2019-09-30 10:59:24 +0800

    ✅引入 dayjs

--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
 import Vue from 'vue'
 import animated from 'animate.css' // css 动画库
 import VueClipboard from 'vue-clipboard2'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
 import App from './App.vue'
 import router from './router/router'
 import store from './store/store'
 
-import './router/control'
+import './router/control' // 路由控制
 import '@/layouts/theme' // 网站主题，样式
 
 import '@/request/mock/mock' // mock模拟请求数据
 
 Vue.use(animated)
 Vue.use(VueClipboard)
+Vue.prototype.$dayjs = dayjs
+dayjs.locale('zh-cn')
 
 Vue.config.productionTip = false
 
