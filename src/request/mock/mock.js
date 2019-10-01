b1c2a6c39a404d2a0e630ef05c8349c9c954e2c2
令狐少侠 <czc12580520@gmail.com>
2019-10-01 21:36:18 +0800

    🔧 修改 mock

--- src/request/mock/mock.js
+++ src/request/mock/mock.js
@@ -1,48 +1,17 @@
 import Mock from 'mockjs'
+import user from './user'
+import notice from './notice'
 
-Mock.mock(/\/login/, 'post', {
-  code: 2000,
-  data: {
-    token: 'login',
-  },
-})
+Mock.mock(/\/login/, 'post', user.login)
 
-Mock.mock(/\/register/, 'post', {
-  code: 2000,
-  data: {
-    token: 'login',
-  },
-})
+Mock.mock(/\/register/, 'post', user.register)
 
-Mock.mock(/\/user_info/, 'get', {
-  code: 2000,
-  data: {
-    nickname: '123456',
-    roles: ['admin'],
-  },
-})
+Mock.mock(/\/user_info/, 'get', user.userInfo)
 
 Mock.mock(/\/user[\s\S]*?/, 'get', () => ({
   data: 1234,
 }))
 
-Mock.mock(/\/notices/, 'get', {
-  code: 2000,
-  data: {
-    notices: [
-      {
-        title: '优惠券即将到期', content: '您有一张八折优惠券即将到期，请及时使用您有一张八折优惠券即将到期，请及时使用您有一张八折优惠券即将到期，请及时使用您有一张八折优惠券即将到期，请及时使用', type: 0, time: 1569600000,
-      },
-      {
-        title: '优惠券即将到期', content: '您有一张八折优惠券即将到期，请及时使用您有一张八折优惠券即将到期，请及时使用您有一张八折优惠券即将到期，请及时使用您有一张八折优惠券即将到期，请及时使用', type: 1, time: 1561910400,
-      },
-      {
-        title: '优惠券即将到期', content: '您有一张八折优惠券即将到期，请及时使用您有一张八折优惠券即将到期，请及时使用您有一张八折优惠券即将到期，请及时使用您有一张八折优惠券即将到期，请及时使用', type: 2, time: 1506700800,
-      },
-    ],
-  },
-})
+Mock.mock(/\/notices/, 'get', notice.notices)
 
-Mock.setup({
-  timeout: '500-1000',
-})
+Mock.setup({ timeout: '500-1000' })
