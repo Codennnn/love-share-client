022df28e3e543894c2c292826b1f3399eee52ecd
令狐少侠 <czc12580520@gmail.com>
2019-10-16 15:09:12 +0800

    ✅ 添加 API

--- src/request/mock/order.js
+++ src/request/mock/order.js
@@ -17,4 +17,30 @@ export default {
       ],
     },
   },
+
+  detail: {
+    code: 2000,
+    data: {
+      detail: {
+        order_id: Random.id(),
+        total: Random.float(0, 100, 2, 2),
+        time: Random.date(),
+        create_time: Random.now('yyyy-MM-dd hh:mm:ss'),
+        buyer_name: Random.cname(),
+        phone: Random.string('number', 11),
+        status: Random.integer(0, 2),
+        address: Random.county(true),
+        note: Random.csentence(0, 20),
+        payment: '微信支付',
+        'goods|1-3': [
+          {
+            goods_id: () => Random.id(),
+            img_urls: [Random.image('480x300'), Random.image('500x300')],
+            name: () => Random.cword(10, 35),
+            price: () => Random.float(0, 100, 2, 2),
+          },
+        ],
+      },
+    },
+  },
 }
