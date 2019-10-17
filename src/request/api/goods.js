7a501362ff7d4d5bdc69836b617a82e0ca06672b
令狐少侠 <czc12580520@gmail.com>
2019-10-17 23:46:08 +0800

    ✅ 添加 API

--- src/request/api/goods.js
+++ src/request/api/goods.js
@@ -27,3 +27,10 @@ export function getGoodsDetail() {
     method: 'get',
   })
 }
+
+export function getGoodsCategory() {
+  return request({
+    url: '/goods/goods_category',
+    method: 'get',
+  })
+}
