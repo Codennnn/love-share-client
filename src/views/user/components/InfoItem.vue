c24c8f8659b1f9e928a5538e0bb91585cf02d19d
令狐少侠 <czc12580520@gmail.com>
2019-10-25 10:44:15 +0800

    ✅ 添加组件

---
+++ src/views/user/components/InfoItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex items-center">
+    <div class="mr-6 w-20 text-right text-gray-600">{{ label }}</div>
+    <span>{{ value }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
