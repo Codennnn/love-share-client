b11a97b9260e7ab0060682e9566b7fc77521b3ba
令狐少侠 <czc12580520@gmail.com>
2019-11-01 23:34:01 +0800

    🚀 功能优化

--- src/views/goods/GoodsCategory.vue
+++ src/views/goods/GoodsCategory.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
+  <div class="pt-3">
     <el-transfer
       v-model="category"
       :titles="titles"
       :props="{key: 'value', lebel: 'value'}"
       :data="categoryList"
+      @left-check-change="leftCheck"
       @change="change"
     >
       <div
@@ -12,21 +13,58 @@
         class="h-full flex justify-center items-center"
       >
         <vs-button
-          class="w-5/12"
+          class="mr-2"
+          color="danger"
           size="small"
-        >添加分类</vs-button>
+          slot="reference"
+          :disabled="disabled"
+          @click="onDeleteCategory()"
+        >删除选中分类</vs-button>
+        <el-popover
+          width="200"
+          trigger="manual"
+          v-model="showPopover"
+        >
+          <div>
+            <vs-input
+              class="w-full"
+              label-placeholder="分类名称"
+              v-model="categoryName"
+            />
+            <div class="mt-2 flex justify-end">
+              <vs-button
+                type="flat"
+                color="#646464"
+                @click="showPopover = false"
+              >取消</vs-button>
+              <vs-button
+                type="flat"
+                @click="onAddCategory()"
+              >确定</vs-button>
+            </div>
+          </div>
+          <vs-button
+            size="small"
+            slot="reference"
+            @click="showPopover = true"
+          >添加分类</vs-button>
+        </el-popover>
       </div>
     </el-transfer>
   </div>
 </template>
 
 <script>
-import { getGoodsCategory } from '@/request/api/goods'
+import { getGoodsCategory, addGoodsCategory, deleteGoodsCategory } from '@/request/api/goods'
 
 export default {
   name: 'GoodsCategory',
   data: () => ({
+    showPopover: false,
+    categoryName: '',
     titles: ['可选分类', '已选分类'],
+    disabled: true,
+    selected: [],
     category: [],
     categoryList: [],
   }),
@@ -50,8 +88,36 @@ export default {
       }
     },
 
+    onAddCategory() {
+      if (this.categoryName.length > 0) {
+        this.categoryList.push({ value: this.categoryName })
+        this.showPopover = false
+        addGoodsCategory()
+      }
+    },
+
+    onDeleteCategory() {
+      this.categoryList.forEach((el, i, _) => {
+        this.selected.forEach((it) => {
+          if (el.value === it) {
+            _.splice(i, 1)
+          }
+        })
+      })
+      deleteGoodsCategory()
+    },
+
+    leftCheck(it) {
+      this.selected = it
+      if (this.selected.length > 0) {
+        this.disabled = false
+      } else {
+        this.disabled = true
+      }
+    },
+
     change() {
-      console.log(this.category)
+      // console.log(this.category)
     },
   },
 }
