3600e1e38ad6073ed865fd80f766c59d6da513ca
令狐少侠 <czc12580520@gmail.com>
2019-11-01 15:56:17 +0800

    🔧 修改配置

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,24 @@
+const plugins = [
+  // 根据官网教程配置的按需引入element组件
+  [
+    'component',
+    {
+      libraryName: 'element-ui',
+      styleLibraryName: 'theme-chalk',
+    },
+  ],
+  // ES 新语法
+  '@babel/plugin-proposal-optional-chaining',
+]
+
+// 生产环境移除 console
+if (process.env.NODE_ENV === 'production') {
+  plugins.push('transform-remove-console')
+}
+
 module.exports = {
   presets: [
     '@vue/app',
   ],
-  // 根据官网教程配置的按需引入element组件
-  plugins: [
-    [
-      'component',
-      {
-        libraryName: 'element-ui',
-        styleLibraryName: 'theme-chalk',
-      },
-    ],
-    '@babel/plugin-proposal-optional-chaining',
-  ],
+  plugins,
 }
