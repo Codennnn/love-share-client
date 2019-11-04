ffbcfce51089266019e87bba5d46b15bc493bc49
令狐少侠 <czc12580520@gmail.com>
2019-11-04 23:34:49 +0800

    🔧 修改 mock 数据

--- src/request/mock/goods.js
+++ src/request/mock/goods.js
@@ -81,6 +81,7 @@ export default {
     data: {
       goods: [
         {
+          goods_id: Random.id(),
           img_urls: ['https://gitee.com/chinesee/images/raw/master/img/img_002.png'],
           name: '一加 OnePlus 7 Pro 2K+90Hz 流体屏 骁龙855旗舰 4800万超广角三摄 6GB+128GB 曜岩灰 全面屏拍照游戏手机',
           price: '22.50',
@@ -88,6 +89,7 @@ export default {
           status: 0,
         },
         {
+          goods_id: Random.id(),
           img_urls: ['https://gitee.com/chinesee/images/raw/master/img/img_002.png'],
           name: 'QCY T1S 5.0真无线蓝牙耳机 Air分离式运动耳麦 运动跑步迷你隐形超小双耳入耳式 苹果/安卓手机通用 黑色',
           price: '52.50',
@@ -95,6 +97,7 @@ export default {
           status: 1,
         },
         {
+          goods_id: Random.id(),
           img_urls: ['https://gitee.com/chinesee/images/raw/master/img/img_001.png'],
           name: 'QCY T1S 5.0真无线蓝牙耳机 Air分离式运动耳麦',
           price: '52.50',
@@ -102,6 +105,7 @@ export default {
           status: 1,
         },
         {
+          goods_id: Random.id(),
           img_urls: ['https://gitee.com/chinesee/images/raw/master/img/img_001.png'],
           name: 'QCY T1S 5.0真无线蓝牙耳机 Air分离式运动耳麦 运动跑步迷你隐形超小双耳入耳式 苹果/安卓手机通用 黑色',
           price: '52.50',
@@ -117,6 +121,7 @@ export default {
     data: {
       goods: [
         {
+          goods_id: Random.id(),
           img_urls: ['https://gitee.com/chinesee/images/raw/master/img/img_003.png'],
           name: '一加 OnePlus 7 Pro 2K+90Hz 流体屏 骁龙855旗舰 4800万超广角三摄 6GB+128GB 曜岩灰 全面屏拍照游戏手机',
           price: '22.50',
