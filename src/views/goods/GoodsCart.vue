1956cf527252130a04e3d9b315634c5ebbf619ca
令狐少侠 <czc12580520@gmail.com>
2019-11-05 18:26:49 +0800

    ✅ 添加组件

---
+++ src/views/goods/GoodsCart.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <el-steps
+      simple
+      :active="currentStep"
+    >
+      <el-step
+        title="购物车"
+        icon="el-icon-shopping-cart-1"
+      ></el-step>
+      <el-step
+        title="收货地址"
+        icon="el-icon-map-location"
+      ></el-step>
+      <el-step
+        title="结算付款"
+        icon="el-icon-wallet"
+      ></el-step>
+    </el-steps>
+
+    <transition
+      enter-active-class="animated fadeIn quickly"
+      leave-active-class="animated fadeOut quickly"
+      mode="out-in"
+    >
+      <keep-alive>
+        <component :is="currentComponent" />
+      </keep-alive>
+    </transition>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+const CartList = Vue.component(
+  'CartList',
+  () => import('./components/CartList.vue'),
+)
+
+export default {
+  name: 'goods-cart',
+  data: () => ({
+    currentStep: 1,
+    currentComponent: 'CartList',
+  }),
+
+  components: { CartList },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/goods/components/CartList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="w-2/3 pr-2">
+    <div>
+      <ul>
+        <li
+          class="p-3 pl-0 bg-white"
+          v-for="(item, i) in cartList"
+          :key="i"
+        ></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cart-list',
+  data: () => ({
+    cartList: [],
+  }),
+
+  mounted() {
+    this.getCartList()
+  },
+
+  methods: {
+    async getCartList() {
+      await this.$store.dispatch('cart/getCartList')
+      console.log(this.$store.getters['cart/carts'])
+      this.cartList = this.$store.getters['cart/carts']
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
