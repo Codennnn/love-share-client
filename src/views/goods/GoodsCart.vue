c63650eb795931a9db460f7d162176f02b11aeb3
令狐少侠 <czc12580520@gmail.com>
2019-11-06 18:32:50 +0800

    🚀 功能优化

--- src/views/goods/GoodsCart.vue
+++ src/views/goods/GoodsCart.vue
@@ -1,20 +1,25 @@
 <template>
   <div>
     <el-steps
+      class="mb-8"
       simple
       :active="currentStep"
     >
       <el-step
         title="购物车"
         icon="el-icon-shopping-cart-1"
+        @click="currentStep = 1, currentComponent = 'CartList'"
       ></el-step>
       <el-step
+        class="cursor-pointer"
         title="收货地址"
         icon="el-icon-map-location"
+        @click.native="currentStep = 2, currentComponent = 'CartAddress'"
       ></el-step>
       <el-step
         title="结算付款"
         icon="el-icon-wallet"
+        @click="currentStep = 3, currentComponent = 'CartSettle'"
       ></el-step>
     </el-steps>
 
@@ -24,7 +29,10 @@
       mode="out-in"
     >
       <keep-alive>
-        <component :is="currentComponent" />
+        <component
+          :is="currentComponent"
+          @switchComponent="changeComponent"
+        />
       </keep-alive>
     </transition>
   </div>
@@ -32,10 +40,15 @@
 
 <script>
 import Vue from 'vue'
+import CartList from './components/CartList.vue'
 
-const CartList = Vue.component(
-  'CartList',
-  () => import('./components/CartList.vue'),
+const CartAddress = Vue.component(
+  'CartAddress',
+  () => import('./components/CartAddress.vue'),
+)
+const CartSettle = Vue.component(
+  'CartSettle',
+  () => import('./components/CartSettle.vue'),
 )
 
 export default {
@@ -45,12 +58,27 @@ export default {
     currentComponent: 'CartList',
   }),
 
-  components: { CartList },
+  components: { CartList, CartAddress, CartSettle },
+
+  methods: {
+    changeComponent(val) {
+      this.currentStep = val.currentStep
+      this.currentComponent = val.currentComponent
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 .quickly {
-  transition: 0.2s;
+  animation-duration: 0.2s;
+}
+
+.el-steps {
+  &::v-deep {
+    .el-step__icon-inner {
+      font-size: 1.6rem !important;
+    }
+  }
 }
 </style>
