20fb67e2b72cbaf46f4a4a2bd961119f392d1f4a
令狐少侠 <czc12580520@gmail.com>
2019-11-08 15:34:49 +0800

    🚀 功能优化

--- src/views/goods/components/CartList.vue
+++ src/views/goods/components/CartList.vue
@@ -1,106 +1,121 @@
 <template>
-  <div class="flex">
-    <div class="w-2/3 pr-2">
-      <div>
-        <ul>
-          <li
-            class="list-item"
-            v-for="(item, i) in cartList"
-            :key="i"
-          >
-            <div class="w-1/3">
-              <el-image
-                class="h-full"
-                fit="cover"
-                :src="item.img_list[0]"
-              />
-            </div>
-
-            <div
-              class="py-4 px-5 flex-col"
-              style="border-right: 1px dashed #cfcfcf;"
+  <div v-if="cartList.length > 0">
+    <div class="flex">
+      <div class="w-2/3 pr-2">
+        <div>
+          <ul>
+            <li
+              class="list-item vs-con-loading__container"
+              v-for="(item, i) in cartList"
+              :id="`li-loading${item.goods_id}`"
+              :key="i"
             >
-              <div class="text-sm">{{ item.name }}</div>
-              <div class="text-sm font-bold">数量</div>
-              <div class="flex">
-                <vs-input-number
-                  :max="item.num"
-                  v-model="number"
+              <div class="w-1/3">
+                <el-image
+                  class="h-full"
+                  fit="cover"
+                  :src="item.img_list[0]"
                 />
               </div>
-            </div>
 
-            <div class="w-1/3 py-4 px-5 flex flex-col items-center">
-              <div class="ml-auto">
-                <vs-chip :color="item.delivery === 1 ? 'primary' : ''">
-                  {{ delivery[item.delivery] }}
-                </vs-chip>
-              </div>
-              <div class="w-full mt-2 mb-4 text-center font-bold">￥{{ item.price }}</div>
               <div
-                class="btn mb-2"
-                style="background: rgb(244, 244, 244);"
+                class="w-1/3 py-4 px-5 flex-col"
+                style="border-right: 1px dashed #cfcfcf;"
               >
-                <i class="el-icon-close mr-1 text-lg"></i>
-                移出购物车
+                <div class="text-sm">{{ item.name }}</div>
+                <div class="text-sm font-bold">数量</div>
+                <div class="flex">
+                  <vs-input-number
+                    :max="item.num"
+                    v-model="number"
+                  />
+                </div>
               </div>
-              <div
-                class="btn text-white"
-                style="background: rgba(var(--vs-primary), 1);"
-              >
-                <i class="el-icon-wallet mr-2 text-lg"></i>
-                结算付款
+
+              <div class="w-1/3 py-4 px-5 flex flex-col items-center">
+                <div class="ml-auto">
+                  <vs-chip :color="item.delivery === 1 ? 'primary' : ''">
+                    {{ delivery[item.delivery] }}
+                  </vs-chip>
+                </div>
+                <div class="w-full mt-2 mb-4 text-center font-bold">￥{{ item.price }}</div>
+                <div
+                  class="btn mb-2"
+                  style="background: rgb(244, 244, 244);"
+                  @click="removeCartItem(item.goods_id)"
+                >
+                  <i class="el-icon-close mr-1 text-lg"></i>
+                  移出购物车
+                </div>
+                <div
+                  class="btn text-white"
+                  style="background: rgba(var(--vs-primary), 1);"
+                >
+                  <i class="el-icon-wallet mr-2 text-lg"></i>
+                  结算付款
+                </div>
               </div>
-            </div>
-          </li>
-        </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="w-1/3 pl-4">
+        <div
+          class="p-4 bg-white rounded-lg"
+          style="box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.1);"
+        >
+          <div class="flex justify-between">
+            <span class="label">优惠券</span>
+            <span>选择</span>
+          </div>
+          <vs-divider />
+          <div>
+            <span class="label">价格详细</span>
+          </div>
+          <div class="item">
+            <span class="item-label">总价</span>
+            <span class="item-value">￥{{ '' }}</span>
+          </div>
+          <div class="item">
+            <span class="item-label">折扣</span>
+            <span class="item-value">￥{{ '' }}</span>
+          </div>
+          <div class="item">
+            <span class="item-label">运费</span>
+            <span class="item-value">￥{{ '0.00' }}</span>
+          </div>
+          <vs-divider />
+          <div class="flex justify-between">
+            <span class="label">实付</span>
+            <span>￥{{ '50.50' }}</span>
+          </div>
+          <vs-button
+            class="w-full mt-4"
+            @click="onSettle()"
+          >开始结算</vs-button>
+        </div>
       </div>
     </div>
+  </div>
 
-    <div class="w-1/3 pl-4">
-      <div
-        class="p-4 bg-white rounded-lg"
-        style="box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.1);"
-      >
-        <div class="flex justify-between">
-          <span class="label">优惠券</span>
-          <span>选择</span>
-        </div>
-        <vs-divider />
-        <div>
-          <span class="label">价格详细</span>
-        </div>
-        <div class="item">
-          <span class="item-label">总价</span>
-          <span class="item-value">￥{{ '' }}</span>
-        </div>
-        <div class="item">
-          <span class="item-label">折扣</span>
-          <span class="item-value">￥{{ '' }}</span>
-        </div>
-        <div class="item">
-          <span class="item-label">运费</span>
-          <span class="item-value">￥{{ '0.00' }}</span>
-        </div>
-        <vs-divider />
-        <div class="flex justify-between">
-          <span class="label">实付</span>
-          <span>￥{{ '50.50' }}</span>
-        </div>
-        <vs-button
-          class="w-full mt-4"
-          @click="onSettle()"
-        >开始结算</vs-button>
-      </div>
-    </div>
+  <div
+    v-else
+    class="p-5 rounded-lg bg-white"
+    style="box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.1);"
+  >
+    <p class="mb-4 text-xl font-bold">您的购物车一件宝贝也有没哎~</p>
+    <vs-button
+      class="text-sm"
+      @click="$router.push('/goods-vivid-list')"
+    >去商城逛一逛</vs-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'cart-list',
+  name: 'CartList',
   data: () => ({
-    cartList: [],
     number: 1,
     delivery: {
       1: '包 邮',
@@ -113,10 +128,24 @@ export default {
     this.getCartList()
   },
 
+  computed: {
+    cartList() {
+      return this.$store.getters['cart/carts']
+    },
+  },
+
   methods: {
     async getCartList() {
       await this.$store.dispatch('cart/getCartList')
-      this.cartList = this.$store.getters['cart/carts']
+    },
+
+    async removeCartItem(id) {
+      this.$vs.loading({
+        container: `#li-loading${id}`,
+        scale: 1,
+      })
+      await this.$store.dispatch('cart/removeCartItem', id)
+      this.$vs.loading.close(`#li-loading${id} > .con-vs-loading`)
     },
 
     onSettle() {
