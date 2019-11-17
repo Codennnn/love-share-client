22b2633b0311c22f3c4168082bcf737738851072
令狐少侠 <czc12580520@gmail.com>
2019-11-17 20:53:43 +0800

    🚀 功能优化

--- src/request/api/common.js
+++ src/request/api/common.js
@@ -7,6 +7,14 @@ export function getNotices() {
   })
 }
 
+export function checkPhoneNumber(data) {
+  return request({
+    url: '/common/check_phone_number',
+    method: 'post',
+    data,
+  })
+}
+
 export function getVerificationCode(data) {
   return request({
     url: '/common/verification_code',
--- src/request/mock/user.js
+++ src/request/mock/user.js
@@ -14,13 +14,6 @@ export default {
     code: 2000,
   },
 
-  verification_code: {
-    code: 2000,
-    data: {
-      code: '884844',
-    },
-  },
-
   userInfo: {
     code: 2000,
     data: {
