fa0dc479768903bbf98af8cf53494958e2e5bb3c
令狐少侠 <czc12580520@gmail.com>
2019-11-17 10:47:08 +0800

    🎨 样式调整

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -1,126 +1,89 @@
 <template>
-  <div class="layout-main">
+  <div id="layout-main">
+    <div id="content-area">
+      <!-- 头部 -->
+      <TheNavbar />
 
-    <!-- 侧边栏 -->
-    <TheSidebar />
-
-    <div
-      id="content-area"
-      :class="{'content-area-full': sidebarCollapse}"
-    >
-      <div class="content-wrapper">
-        <!-- 回到顶部按钮 -->
-        <BackToTop
-          bottom="5%"
-          visibleoffset="500"
-        >
-          <vs-button
-            icon="arrow_upward"
-            class="shadow-lg"
-          />
-        </BackToTop>
-
-        <!-- 头部 -->
-        <TheNavbar />
-
-        <div class="router-view px-6">
-          <div class="router-content pt-24">
-            <!-- 面包屑导航 -->
-            <transition
-              enter-active-class="animated zoomIn faster"
-              leave-active-class="animated zoomOut faster"
-              mode="out-in"
+      <div class="router-view px-6">
+        <div class="router-content pt-24">
+          <!-- 面包屑导航 -->
+          <transition
+            enter-active-class="animated zoomIn faster"
+            leave-active-class="animated zoomOut faster"
+            mode="out-in"
+          >
+            <div
+              class="flex items-center mt-1 ml-1 mb-3"
+              v-if="$route.meta.breadcrumb"
             >
-              <div
-                class="flex items-center mt-1 ml-1 mb-3"
-                v-if="$route.meta.breadcrumb"
-              >
-                <i
-                  class="iconfont icon-back"
-                  @click="$router.go(-1)"
-                ></i>
-                <vs-breadcrumb
-                  separator="chevron_right"
-                  :items="$route.meta.breadcrumb"
-                ></vs-breadcrumb>
-              </div>
-            </transition>
+              <i
+                class="iconfont icon-back"
+                @click="$router.go(-1)"
+              ></i>
+              <vs-breadcrumb
+                separator="chevron_right"
+                :items="$route.meta.breadcrumb"
+              ></vs-breadcrumb>
+            </div>
+          </transition>
 
-            <!-- 主区域 -->
-            <transition
-              enter-active-class="animated fadeIn faster"
-              leave-active-class="animated fadeOut faster"
-              mode="out-in"
-            >
-              <router-view />
-            </transition>
-          </div>
-
-          <!-- 页脚 -->
-          <TheFooter class="router-footer" />
+          <!-- 主区域 -->
+          <transition
+            enter-active-class="animated fadeIn faster"
+            leave-active-class="animated fadeOut faster"
+            mode="out-in"
+          >
+            <router-view />
+          </transition>
         </div>
+
+        <!-- 页脚 -->
+        <TheFooter class="router-footer" />
       </div>
+
+      <!-- 回到顶部按钮 -->
+      <BackToTop
+        bottom="5%"
+        visibleoffset="500"
+      >
+        <vs-button
+          icon="arrow_upward"
+          class="shadow-lg"
+        />
+      </BackToTop>
     </div>
   </div>
 </template>
 
 <script>
 import BackToTop from 'vue-backtotop'
-import TheSidebar from './components/TheSidebar.vue'
 import TheNavbar from './components/TheNavbar.vue'
 import TheFooter from './components/TheFooter.vue'
 
 export default {
   name: 'Main',
   components: {
-    TheSidebar,
     TheNavbar,
     TheFooter,
     BackToTop,
-  },
-
-  computed: {
-    sidebarCollapse() {
-      return this.$store.state.sidebarCollapse
-    },
   },
 }
 </script>
 
 <style lang="scss" scoped>
-.layout-main {
-  position: relative;
-  height: 100%;
-  background: rgb(248, 248, 248);
-}
+$footer-height: 65px;
 
-#content-area {
-  min-height: 100%;
+#layout-main {
+  position: relative;
   min-width: 900px;
-  margin-left: $side-bar-width;
-  transition: margin-left 0.5s;
-  &.content-area-full {
-    margin-left: 65px;
-  }
-  .content-wrapper {
-    min-height: 100vh;
-  }
+  background: rgb(248, 248, 248);
 }
 
 .router-view {
   position: relative;
   min-height: 100vh;
   .router-content {
-    min-height: calc(100vh - 3rem - #{$footer-height});
-    .icon-back {
-      cursor: pointer;
-      font-size: 30px;
-      color: rgb(133, 133, 133);
-      transition: all 0.3s;
-      &:hover {
-        color: #333;
-      }
-    }
+    min-height: calc(100vh - #{$footer-height});
   }
   .router-footer {
     height: $footer-height;
