641916bc5ff282a1bab0d8dc375d0ca17c6850b7
令狐少侠 <czc12580520@gmail.com>
2019-11-18 22:51:24 +0800

    🚀 功能优化

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import animated from 'animate.css' // css 动画库
 import VueClipboard from 'vue-clipboard2' // 粘贴板
 import dayjs from 'dayjs' // 日期格式化
 import App from './App.vue'
-import router from './router/router'
 import store from './store/store'
+import router from './router/router'
 
 import 'dayjs/locale/zh-cn' // dayjs 语言包
 import './router/control' // 路由控制
@@ -12,23 +12,24 @@ import '@/layouts/theme' // 网站主题，样式
 
 import '@/request/mock/mock' // 本地 mock 模拟请求数据
 
+import { getToken } from '@/utils/token'
+
 Vue.use(animated)
 Vue.use(VueClipboard)
 Vue.prototype.$dayjs = dayjs
 dayjs.locale('zh-cn')
 
-Vue.directive('user', {
+Vue.directive('login', {
   inserted(el) {
-    const { token } = store.state.user
-
-    if (token.length <= 0) {
+    const token = getToken()
+    if (!token) {
       el.parentNode.removeChild(el) // 如果没有权限，则移除节点
     }
   },
 })
-Vue.prototype.$user = () => {
-  const { token } = store.state.user
-  if (token.length > 0) {
+Vue.prototype.$login = () => {
+  const token = getToken()
+  if (token) {
     return true
   }
   return false
