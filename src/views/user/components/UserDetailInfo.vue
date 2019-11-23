b89d20d8c1faab2b85671a199c0a980e8101b16b
令狐少侠 <czc12580520@gmail.com>
2019-11-23 17:01:32 +0800

    🚀 功能优化

--- src/views/user/components/UserDetailInfo.vue
+++ src/views/user/components/UserDetailInfo.vue
@@ -56,121 +56,7 @@
 
       <!-- 收货地址 -->
       <div class="w-1/3">
-        <vs-list>
-          <vs-list-header title="收货地址"></vs-list-header>
-          <template v-if="addressList.length > 0">
-            <vs-list-item
-              v-for="(item, i) in addressList"
-              :key="i"
-              :title="`${item.receiver} ${item.phone}`"
-              :subtitle="item.address"
-            >
-              <div class="flex items-center">
-                <vs-chip
-                  color="primary"
-                  v-if="item.address_id === defaultAddress"
-                >默认</vs-chip>
-                <vs-dropdown vs-trigger-click>
-                  <i class="el-icon-more ml-2 cursor-pointer"></i>
-                  <vs-dropdown-menu class="w-24">
-                    <vs-dropdown-item
-                      v-if="!(item.address_id === defaultAddress)"
-                      class="text-center"
-                      @click="setDefaultAddress(item.address_id)"
-                    >
-                      设为默认
-                    </vs-dropdown-item>
-                    <vs-dropdown-item
-                      class="text-center"
-                      @click="editForm = true, onShowForm(item)"
-                    >
-                      修改
-                    </vs-dropdown-item>
-                    <vs-dropdown-item
-                      class="text-center text-danger"
-                      divider
-                      @click="onDeleteAddress(item.address_id)"
-                    >
-                      删除
-                    </vs-dropdown-item>
-                  </vs-dropdown-menu>
-                </vs-dropdown>
-              </div>
-            </vs-list-item>
-          </template>
-          <div
-            class="my-4 text-gray-600 text-center"
-            v-else
-          >暂无收货地址</div>
-        </vs-list>
-        <div
-          class="mt-2 p-3 rounded-lg"
-          style="background: rgba(150, 150, 150, 0.1);"
-        >
-          <div
-            class="my-3"
-            v-if="showForm"
-          >
-            <div class="flex">
-              <vs-input
-                class="mr-2"
-                label="收货人"
-                size="small"
-                v-model="addressData.receiver"
-                val-icon-warning="warning"
-                :warning="receiverWarning"
-                @focus="() => { receiverWarning = false }"
-              />
-              <vs-input
-                label="联系电话"
-                size="small"
-                val-icon-warning="warning"
-                v-model="addressData.phone"
-                :warning="phoneWarning"
-                @focus="() => { phoneWarning = false }"
-              />
-            </div>
-            <vs-input
-              class="w-full"
-              label="地址"
-              size="small"
-              val-icon-warning="warning"
-              v-model="addressData.address"
-              :warning="addressWarning"
-              @focus="() => { addressWarning = false }"
-            />
-            <vs-select
-              class="address-select"
-              label="地址类型"
-              v-model="addressData.address_type"
-              :warning="addressTypeWarning"
-              @focus="() => { addressTypeWarning = false }"
-            >
-              <vs-select-item
-                v-for="(item, i) in ['学校', '家庭', '公司']"
-                :key="i"
-                :value="item"
-                :text="item"
-              />
-            </vs-select>
-          </div>
-          <div class="flex items-center justify-end">
-            <vs-button
-              class="mr-2 text-xl"
-              v-if="showForm"
-              type="border"
-              icon="done"
-              @click="editForm ? onModifyAddress() : onAddAddress()"
-            ></vs-button>
-            <vs-button
-              class="text-xl"
-              :color="showForm ? 'danger' : 'primary'"
-              type="border"
-              :icon="showForm ? 'clear' : 'add'"
-              @click="showForm ? onHideForm() : onShowForm()"
-            ></vs-button>
-          </div>
-        </div>
+        <Address />
       </div>
     </div>
 
@@ -184,18 +70,11 @@
 
 <script>
 import Vue from 'vue'
-import _cloneDeepWith from 'lodash/cloneDeepWith'
 import InfoItem from './user-detail-info/InfoItem.vue'
+import Address from './user-detail-info/Address.vue'
 import { setCreditColor } from '@/utils/util'
 
-import {
-  getUserDetail,
-  getAddressList,
-  setDefaultAddress,
-  addAddress,
-  deleteAddress,
-  modifyAddress,
-} from '@/request/api/user'
+import { getUserDetail } from '@/request/api/user'
 
 const EditUserInfo = Vue.component(
   'EditUserInfo',
@@ -204,34 +83,16 @@ const EditUserInfo = Vue.component(
 
 export default {
   name: 'UserDetailInfo',
-  components: { InfoItem, EditUserInfo },
+  components: { InfoItem, Address, EditUserInfo },
 
   data: () => ({
     setCreditColor,
     showSidebar: false,
     detail: {},
-    addressList: [], // 地址列表
-    defaultAddress: null,
-    showForm: false,
-    editForm: false,
-
-    addressData: {
-      address_id: '', // 地址 ID
-      receiver: '', // 收货人
-      phone: '', // 联系电话
-      address: '', // 地址
-      address_type: '', // 地址类型
-    },
-
-    receiverWarning: false,
-    phoneWarning: false,
-    addressWarning: false,
-    addressTypeWarning: false,
   }),
 
   created() {
     this.getUserDetail()
-    this.getAddressList()
   },
 
   methods: {
@@ -241,122 +102,6 @@ export default {
         this.detail = data.user_detail
       }
     },
-
-    // 获取收货地址
-    async getAddressList() {
-      const { code, data } = await getAddressList()
-      if (code === 2000) {
-        this.defaultAddress = data.default_address
-        this.addressList = data.address_list
-      }
-    },
-
-    // 验证
-    verification() {
-      if (this.addressData.receiver.length <= 0) {
-        this.receiverWarning = true
-      }
-      if (this.addressData.phone.length <= 0) {
-        this.phoneWarning = true
-      }
-      if (this.addressData.address.length <= 0) {
-        this.addressWarning = true
-      }
-      if (this.addressData.address_type.length <= 0) {
-        this.addressTypeWarning = true
-      }
-      if (
-        this.addressData.receiver.length > 0
-        && this.addressData.phone.length > 0
-        && this.addressData.address.length > 0
-        && this.addressData.address_type.length > 0
-      ) {
-        return true
-      }
-      return false
-    },
-
-    // 显示地址表单
-    onShowForm(item) {
-      if (this.editForm) {
-        this.addressData = _cloneDeepWith(item)
-      }
-      this.showForm = true
-    },
-
-    // 隐藏地址表单
-    onHideForm() {
-      this.showForm = false
-
-      this.addressData.address_id = ''
-      this.addressData.receiver = ''
-      this.addressData.phone = ''
-      this.addressData.address = ''
-      this.addressData.address_type = ''
-      this.receiverWarning = false
-      this.phoneWarning = false
-      this.addressWarning = false
-      this.addressTypeWarning = false
-      this.editForm = false
-    },
-
-    // 删除地址
-    async onDeleteAddress(id) {
-      this.addressList.forEach((el, i, _this) => {
-        if (el.address_id === id) {
-          _this.splice(i, 1)
-        }
-      })
-      await deleteAddress()
-    },
-
-    // 添加地址
-    async onAddAddress() {
-      if (this.verification()) {
-        this.addressList.push(_cloneDeepWith(this.addressData))
-        this.onHideForm()
-        const { code, data } = await addAddress()
-        if (code === 2000) {
-          this.addressData.address_id = data.address_id
-        }
-      }
-    },
-
-    // 修改地址
-    async onModifyAddress() {
-      if (this.verification()) {
-        this.addressList.forEach((el, i, _) => {
-          if (el.address_id === this.addressData.address_id) {
-            _.splice(i, 1, _cloneDeepWith(this.addressData))
-          }
-        })
-        this.onHideForm()
-        const { code } = await modifyAddress()
-        if (code === 2000) {
-          // TODO
-        }
-      }
-    },
-
-    // 设置默认地址
-    async setDefaultAddress(id) {
-      this.defaultAddress = id
-      await setDefaultAddress()
-    },
   },
 }
 </script>
-
-<style lang="scss" scoped>
-.address-select {
-  &::v-deep {
-    .vs-select--input {
-      border: 1px solid rgba(0, 0, 0, 0.2);
-      font-size: 88%;
-    }
-    &.input-select-validate-warning .input-select {
-      border-color: rgba(var(--vs-warning), 1);
-    }
-  }
-}
-</style>
