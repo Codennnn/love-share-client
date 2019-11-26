6a38008a866a9c1472f67d2bc8b3a10989664d46
令狐少侠 <czc12580520@gmail.com>
2019-11-26 15:23:06 +0800

    🔧 修改 vuex

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,9 +1,14 @@
 import { setToken, getToken, removeToken } from '@/utils/token'
-import { signIn, signOut, getUserInfo } from '@/request/api/user'
+import {
+  signIn, signOut, getUserInfo, getAddressList, addAddress,
+  deleteAddress, updateAddress, setDefaultAddress,
+} from '@/request/api/user'
 
 const state = {
   token: getToken(),
   info: {},
+  defaultAddress: '',
+  addressList: [],
 }
 
 const mutations = {
@@ -15,6 +20,12 @@ const mutations = {
   },
   SET_AVATAR(state, url) {
     state.info.avatar_url = url
+  },
+  SET_ADDRESS(state, list) {
+    state.addressList = list
+  },
+  SET_DEFAULT_ADDRESS(state, id) {
+    state.defaultAddress = id
   },
 }
 
@@ -28,6 +39,13 @@ const actions = {
     return code
   },
 
+  async signOut({ commit, state }) {
+    await signOut(state.token)
+    commit('SET_TOKEN', '')
+    commit('SET_INFO', {})
+    removeToken() // 移除本地token缓存
+  },
+
   async getUserInfo({ commit }) {
     try {
       const { code, data } = await getUserInfo()
@@ -39,11 +57,40 @@ const actions = {
     }
   },
 
-  async signOut({ commit, state }) {
-    await signOut(state.token)
-    commit('SET_TOKEN', '')
-    commit('SET_INFO', {})
-    removeToken() // 移除本地token缓存
+  async getAddressList({ commit }) {
+    const { code, data } = await getAddressList()
+    if (code === 2000) {
+      commit('SET_ADDRESS', data.address_list)
+      commit('SET_DEFAULT_ADDRESS', data.default_address)
+    }
+  },
+
+  async addAddress({ dispatch }, data) {
+    const { code } = await addAddress(data)
+    if (code === 2000) {
+      await dispatch('getAddressList')
+    }
+  },
+
+  async deleteAddress({ dispatch }, id) {
+    const { code } = await deleteAddress({ address_id: id })
+    if (code === 2000) {
+      await dispatch('getAddressList')
+    }
+  },
+
+  async updateAddress({ dispatch }, data) {
+    const { code } = await updateAddress(data)
+    if (code === 2000) {
+      await dispatch('getAddressList')
+    }
+  },
+
+  async setDefaultAddress({ commit }, id) {
+    const { code } = await setDefaultAddress({ address_id: id })
+    if (code === 2000) {
+      commit('SET_DEFAULT_ADDRESS', id)
+    }
   },
 }
 
