c270f407190e69a656006b4aa53e8d3740d9baeb
令狐少侠 <czc12580520@gmail.com>
2019-11-30 15:16:36 +0800

    🔧 修改 vuex

--- src/store/modules/chat.js
+++ src/store/modules/chat.js
@@ -1,124 +1,11 @@
 import Vue from 'vue'
 
-import { getContactList } from '@/request/api/user'
+import { getContactList, getChatData } from '@/request/api/user'
 
 const state = {
   contactList: [],
   chatSearchQuery: '',
-  chats: {
-    1: {
-      isPinned: false,
-      msg: [
-        {
-          textContent: '我们能帮你什么吗？我们在这里为您服务！',
-          time: 'Mon Dec 10 2018 07:45:00 GMT+0000 (GMT)',
-          isSent: true,
-          isSeen: true,
-        },
-        {
-          textContent: 'Hey John, I am looking for the best admin template. Could you please help me to find it out?Hey John, I am looking for the best admin template. Could you please help me to find it out?Hey John, I am looking for the best admin template. Could you please help me to find it out?Hey John, I am looking for the best admin template. Could you please help me to find it out?',
-          time: 'Mon Dec 10 2018 07:45:23 GMT+0000 (GMT)',
-          isSent: false,
-          isSeen: true,
-        },
-        {
-          textContent: 'It should be Bootstrap 4 compatible.',
-          time: 'Mon Dec 10 2018 07:45:55 GMT+0000 (GMT)',
-          isSent: false,
-          isSeen: true,
-        },
-        {
-          textContent: 'Absolutely!',
-          time: 'Mon Dec 10 2018 07:46:00 GMT+0000 (GMT)',
-          isSent: true,
-          isSeen: true,
-        },
-        {
-          textContent: 'Modern admin is the responsive bootstrap 4 admin template.!',
-          time: 'Mon Dec 10 2018 07:46:05 GMT+0000 (GMT)',
-          isSent: true,
-          isSeen: true,
-        },
-        {
-          textContent: 'Looks clean and fresh UI.',
-          time: 'Mon Dec 10 2018 07:46:23 GMT+0000 (GMT)',
-          isSent: false,
-          isSeen: true,
-        },
-        {
-          textContent: 'It\'s perfect for my next project.',
-          time: 'Mon Dec 10 2018 07:46:33 GMT+0000 (GMT)',
-          isSent: false,
-          isSeen: true,
-        },
-        {
-          textContent: 'How can I purchase it?',
-          time: 'Mon Dec 10 2018 07:46:43 GMT+0000 (GMT)',
-          isSent: false,
-          isSeen: true,
-        },
-        {
-          textContent: 'Thanks, from ThemeForest.',
-          time: 'Mon Dec 10 2018 07:46:53 GMT+0000 (GMT)',
-          isSent: true,
-          isSeen: true,
-        },
-        {
-          textContent: 'I will purchase it for sure.',
-          time: 'Mon Dec 10 2018 07:47:00 GMT+0000 (GMT)',
-          isSent: false,
-          isSeen: true,
-        },
-        {
-          textContent: 'Thanks',
-          time: 'Mon Dec 10 2018 07:47:05 GMT+0000 (GMT)',
-          isSent: false,
-          isSeen: false,
-        },
-      ],
-    },
-    2: {
-      isPinned: true,
-      msg: [
-        {
-          textContent: 'Hi',
-          time: 'Mon Dec 10 2018 07:45:00 GMT+0000 (GMT)',
-          isSent: true,
-          isSeen: true,
-        },
-        {
-          textContent: 'Hello. How can I help You?',
-          time: 'Mon Dec 11 2018 07:45:15 GMT+0000 (GMT)',
-          isSent: false,
-          isSeen: true,
-        },
-        {
-          textContent: 'Can I get details of my last transaction I made last month?',
-          time: 'Mon Dec 11 2018 07:46:10 GMT+0000 (GMT)',
-          isSent: true,
-          isSeen: true,
-        },
-        {
-          textContent: 'We need to check if we can provide you such information.',
-          time: 'Mon Dec 11 2018 07:45:15 GMT+0000 (GMT)',
-          isSent: false,
-          isSeen: true,
-        },
-        {
-          textContent: 'I will inform you as I get update on this.',
-          time: 'Mon Dec 11 2018 07:46:15 GMT+0000 (GMT)',
-          isSent: false,
-          isSeen: true,
-        },
-        {
-          textContent: 'Thank You',
-          time: 'Mon Dec 11 2018 07:46:20 GMT+0000 (GMT)',
-          isSent: true,
-          isSeen: false,
-        },
-      ],
-    },
-  },
+  chats: {},
   showChatbox: false, // 是否显示聊天框
 }
 
@@ -127,9 +14,13 @@ const mutations = {
     state.contactList = contactList
   },
 
+  SET_CHAT_DATA(state, data) {
+    state.chats = data
+  },
+
   SEND_CHAT_MESSAGE(state, payload) {
     if (payload.chatData) {
-      state.chats[Object.keys(state.chats).find(key => key === payload.id)].msg.push(payload.msg)
+      state.chats[payload.id].msg.push(payload.msg)
     } else {
       const chatId = payload.id
       Vue.set(state.chats, [chatId], { isPinned: payload.isPinned, msg: [payload.msg] })
@@ -146,7 +37,7 @@ const mutations = {
 
   MARK_SEEN_ALL_MESSAGES(state, payload) {
     payload.chatData.msg.forEach((msg) => {
-      msg.isSeen = true
+      msg.is_seen = true
     })
   },
 }
@@ -156,6 +47,13 @@ const actions = {
     const { code, data } = await getContactList()
     if (code === 2000) {
       commit('SET_CONTACT_LIST', data.contact_list)
+    }
+  },
+
+  async getChatData({ commit }) {
+    const { code, data } = await getChatData()
+    if (code === 2000) {
+      commit('SET_CHAT_DATA', data.chat_data)
     }
   },
 
@@ -178,7 +76,7 @@ export default {
   mutations,
   actions,
   getters: {
-    chatDataOfUser: state => id => state.chats[Object.keys(state.chats).find(key => key === id)],
+    chatDataOfUser: state => id => state.chats[id],
 
     getContactList: (state, getters) => {
       const chatArray = state.contactList.filter((contact) => {
@@ -215,7 +113,7 @@ export default {
       const chatData = getters.chatDataOfUser(id)
       if (chatData) {
         chatData.msg.map((msg) => {
-          if (!msg.isSeen && !msg.isSent) {
+          if (!msg.is_seen && !msg.isSent) {
             unseenMsg += 1
           }
           return ''
