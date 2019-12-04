d9f08635df1767d95c1ededd20291ba32c5bc16c
令狐少侠 <czc12580520@gmail.com>
2019-12-04 17:16:55 +0800

    🎨 样式调整

--- src/views/error-page/NotFound.vue
+++ src/views/error-page/NotFound.vue
@@ -2,8 +2,8 @@
   <div class="h-screen flex justify-center items-center bg-white">
     <div class="flex items-center">
       <div style="width: 15rem; margin:0 3rem; ">
-        <p class="text-2xl font-bold">Life Was Like A Box Of Chocolates. </p>
-        <p class="my-6 text-primary text-sm">You Never Know What You're Gonna Get...</p>
+        <p class="text-2xl font-bold">生活就像一盒巧克力 </p>
+        <p class="mt-2 mb-8 text-primary text-sm">你永远不知道下一颗是什么味道...</p>
         <vs-button to="Home">迷路了，请带我回家</vs-button>
       </div>
       <img
