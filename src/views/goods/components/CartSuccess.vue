4d7a77fc8fe269f576b7ec0b265f98e2f6a87380
令狐少侠 <czc12580520@gmail.com>
2019-12-13 16:23:58 +0800

    ✅ 添加组件

---
+++ src/views/goods/components/CartSuccess.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="p-4">
+    <div class="p-10 flex flex-col items-center justify-center base-shadow bg-white rounded-lg">
+      <i
+        class="el-icon-success text-success"
+        style="font-size: 80px;"
+      ></i>
+      <p class="mt-2 mb-6 text-2xl">付款成功</p>
+      <div>
+        <vs-button
+          class="mr-4"
+          size="small"
+          color="#999"
+          type="border"
+        >返回首页</vs-button>
+        <vs-button
+          color="#999"
+          size="small"
+        >查看订单</vs-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CartSuccess',
+}
+</script>
