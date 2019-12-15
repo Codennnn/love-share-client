248ef16a9c965f2bf1a5481d67316237b1e47441
令狐少侠 <czc12580520@gmail.com>
2019-12-15 22:19:14 +0800

    ✅ 添加组件

---
+++ src/views/user/UserFollows.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="p-6 bg-white rounded-lg">
+    <div class="mb-4 flex">
+      <vs-button
+        color="#999"
+        type="flat"
+        size="large"
+        icon="arrow_back"
+        @click="$router.go(-1)"
+      >返回</vs-button>
+    </div>
+    <div class="w-1/3">
+      <vs-list>
+        <vs-list-header
+          title="关注的人"
+          color="primary"
+        ></vs-list-header>
+        <template v-if="follows.length > 0">
+          <vs-list-item
+            v-for="item in follows"
+            :key="item._id"
+            :title="item.nickname"
+            :subtitle="item.introduction"
+          >
+            <template slot="avatar">
+              <vs-avatar :src="`${item.avatar_url}?imageView2/2/w/50`" />
+            </template>
+          </vs-list-item>
+        </template>
+        <div v-else>
+          <div class="p-3 text-gray-600 text-center">暂无关注任何人</div>
+        </div>
+      </vs-list>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getUserFollows } from '@/request/api/user'
+
+export default {
+  name: 'UserFollows',
+  data: () => ({
+    follows: [],
+  }),
+
+  created() {
+    this.getUserFollows()
+  },
+
+  methods: {
+    async getUserFollows() {
+      const { code, data } = await getUserFollows()
+      if (code === 2000) {
+        this.follows = data.follows
+      }
+    },
+  },
+}
+</script>
