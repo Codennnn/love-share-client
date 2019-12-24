7835cbf712530f6d91e2b02a795836164fb631cb
令狐少侠 <czc12580520@gmail.com>
2019-12-24 17:14:22 +0800

    ⚡️ 功能优化

--- src/utils/util.js
+++ src/utils/util.js
@@ -16,7 +16,11 @@ export function timeDiff(time) {
     if (diff === 0) {
       const diffHour = now.diff(date, 'hour')
       if (diffHour <= 1) {
-        return `${now.diff(date, 'minute')}分钟前`
+        const diffMin = now.diff(date, 'minute')
+        if (diffMin < 1) {
+          return `${now.diff(date, 'second')}秒前`
+        }
+        return `${diffMin}分钟前`
       }
       if (diffHour < 24) {
         return `${diffHour}小时前`
--- src/views/user/components/list/UserCollections.vue
+++ src/views/user/components/list/UserCollections.vue
@@ -13,18 +13,21 @@
       >
         <vs-button
           radius
+          class="text-xl"
           type="flat"
           color="#999"
           icon="arrow_back"
           @click="$router.go(-1)"
         ></vs-button>
-        <p class="ml-2 font-bold text-gray-600">收藏夹</p>
+        <p class="ml-1 font-bold text-gray-600">收藏夹</p>
       </div>
 
       <template slot="thead">
+        <vs-th>图片</vs-th>
         <vs-th>商品名称</vs-th>
         <vs-th>求购价</vs-th>
         <vs-th>收藏于</vs-th>
+        <vs-th>操作</vs-th>
       </template>
 
       <template slot-scope="{data}">
@@ -32,6 +35,12 @@
           v-for="(tr, i) in data"
           :key="i"
         >
+          <vs-td>
+            <vs-image
+              class="w-24 h-24 base-shadow"
+              :src="`${tr.goods.img_list[0]}?imageView2/2/w/100`"
+            ></vs-image>
+          </vs-td>
           <vs-td>{{ tr.goods.name }}</vs-td>
           <vs-td>
             <p class="text-gray-600 font-bold">￥{{ Number(tr.goods.price).toFixed(2) }}</p>
@@ -40,7 +49,26 @@
             <p class="text-gray-600">{{ timeDiff(tr.created_at) }}</p>
           </vs-td>
           <vs-td>
-            <i class="el-icon-more-outline"></i>
+            <div class="flex">
+              <div
+                v-if="isInCollections(tr.goods._id)"
+                class="py-2 flex justify-center items-center bg-gray-100 cursor-pointer rounded-lg"
+                style="width: 7.5rem;"
+                @click="deleteCollection(tr.goods._id)"
+              >
+                <i class="el-icon-star-on text-warning text-xl"></i>
+                <p class="ml-1 text-gray-600">取消收藏</p>
+              </div>
+              <div
+                v-else
+                class="py-2 flex justify-center items-center bg-gray-100 cursor-pointer rounded-lg"
+                style="width: 7.5rem;"
+                @click="addCollection(tr.goods._id)"
+              >
+                <i class="el-icon-star-off text-gray-600 text-lg"></i>
+                <p class="ml-2 text-gray-600">收藏</p>
+              </div>
+            </div>
           </vs-td>
         </vs-tr>
       </template>
@@ -49,26 +77,54 @@
 </template>
 
 <script>
+import Vue from 'vue'
 import { timeDiff } from '@/utils/util'
 
-import { getCollectionList } from '@/request/api/user'
+import { getCollectionList, addCollection, deleteCollection } from '@/request/api/user'
 
 export default {
   name: 'UserCollections',
   data: () => ({
     timeDiff,
     collections: [],
+    ids: {},
   }),
+
+  computed: {
+    isInCollections() {
+      return id => Object.prototype.hasOwnProperty.call(this.ids, id)
+    },
+  },
 
   created() {
     this.getCollectionList()
   },
 
   methods: {
+    // 获取收藏列表
     async getCollectionList() {
       const { code, data } = await getCollectionList()
       if (code === 2000) {
+        data.collections.forEach((el) => {
+          this.ids[el.goods._id] = el.goods._id
+        })
         this.collections = data.collections
+      }
+    },
+
+    // 收藏商品
+    async addCollection(goods_id) {
+      const { code } = await addCollection({ goods_id })
+      if (code === 2000) {
+        Vue.set(this.ids, goods_id)
+      }
+    },
+
+    // 取消收藏商品
+    async deleteCollection(goods_id) {
+      const { code } = await deleteCollection({ goods_id })
+      if (code === 2000) {
+        Vue.delete(this.ids, goods_id)
       }
     },
   },
