58c7a3c3a6a3658ecca03e899deedd277b7b7c13
令狐少侠 <czc12580520@gmail.com>
2019-12-25 19:36:11 +0800

    ✅ 添加组件

---
+++ src/views/goods/components/DetailSettle.vue
@@ -0,0 +1,17 @@
+<template>
+  <vs-popup
+    fullscreen
+    title="确认购买信息"
+    :active.sync="showPopup"
+  >
+  </vs-popup>
+</template>
+
+<script>
+export default {
+  name: 'DetailSettle',
+  data: () => ({
+    showPopup: true,
+  }),
+}
+</script>
