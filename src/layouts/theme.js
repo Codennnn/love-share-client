f660ab9778f0da8e723b29709df1d73873b57178
令狐少侠 <czc12580520@gmail.com>
2019-12-30 12:33:20 +0800

    🎨 样式调整

--- src/layouts/theme.js
+++ src/layouts/theme.js
@@ -24,11 +24,11 @@ Vue.use(Vuesax, {
   theme: {
     colors: {
       primary: '#5B8FFF',
-      // success: '#28C76F',
-      success: '#24c1a0',
+      success: '#28C76F',
       danger: '#EA5455',
       warning: '#FF9F43',
       dark: '#1E1E1E',
+      green: '#24c1a0',
     },
   },
 })
