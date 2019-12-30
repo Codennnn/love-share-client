78aec23cf30be1636e4d01cea78b99d0eb05f0ce
令狐少侠 <czc12580520@gmail.com>
2019-12-30 17:19:55 +0800

    ⚡️ 功能优化

--- src/views/goods/components/DetailComment.vue
+++ src/views/goods/components/DetailComment.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="p-3 bg-white rounded-lg">
+  <div
+    ref="comment"
+    class="relative p-3 bg-white rounded-lg overflow-hidden"
+    style="max-height: 500px;"
+  >
     <div class="mb-1 flex justify-between items-center">
       <span class="mb-2 text-gray-600">留言板</span>
       <vs-button
@@ -16,121 +20,138 @@
         :counter-danger.sync="counterDanger"
       />
     </div>
-    <ul
+
+    <VuePerfectScrollbar
       v-if="comments.length > 0"
-      class="mt-6 pt-2"
+      style="height: 350px;"
+      :settings="{
+          maxScrollbarLength: 160,
+          wheelSpeed: 0.60,
+        }"
     >
-      <li
-        class="msg relative mb-2 overflow-hidden"
-        style="transition: all 0.3s;"
-        v-for="(cm, i) in comments"
-        :key="i"
-        :class="{'px-1 py-2 bg-gray-100 rounded-lg': currMsg === cm._id}"
-      >
-        <div class="flex items-center">
-          <vs-avatar :src="`${cm.sender.avatar_url}?imageView2/2/w/50`"></vs-avatar>
-          <p class="name ml-1">
-            {{ cm.sender.nickname }}
-          </p>
-          <p
-            v-if="cm.sender._id === owner"
-            class="owner ml-1"
-          >主人</p>
-          <p class="ml-2 text-xs text-gray-500">
-            {{ timeDiff(cm.created_at) }}
-          </p>
-        </div>
-        <p class="ml-8 text-sm text-gray-600">{{ cm.content }}</p>
-        <p
-          class="reply absolute cursor-pointer"
-          @click="showReplyInput(cm._id, cm.sender, 1)"
-        >回复</p>
-        <ul
-          v-if="cm.replies.length > 0"
-          class="ml-6 p-2 bg-gray-100 rounded-lg"
+      <ul class="mt-6 pt-2 pb-10">
+        <li
+          class="msg relative mb-2 overflow-hidden"
+          style="transition: all 0.3s;"
+          v-for="(cm, i) in comments"
+          :key="i"
+          :class="{'px-1 py-2 bg-gray-100 rounded-lg': currMsg === cm._id}"
         >
-          <li
-            v-for="(it, index) in cm.replies"
-            :key="index"
-            class="reply-item"
-          >
-            <p class="text-sm">
-              <span class="name cursor-pointer">
-                {{ it.sender.nickname }}
-                <span
-                  v-if="it.sender._id === owner"
-                  class="owner"
-                >主人</span>
-              </span>
-              <span class="mx-1 text-gray-500">回复</span>
-              <span class="name cursor-pointer">
-                {{ it.at.nickname }}
-                <span
-                  v-if="it.sender._id === owner"
-                  class="owner"
-                >主人</span>
-                :
-              </span>
-              <span class="text-gray-500">
-                {{ it.content }}
-              </span>
-              <i
-                title="回复"
-                class="reply-icon el-icon-chat-dot-square ml-2
-                 cursor-pointer text-base text-gray-600"
-                @click="showReplyInput(cm._id, it.at, 2)"
-              ></i>
+          <div class="flex items-center">
+            <vs-avatar :src="`${cm.sender.avatar_url}?imageView2/2/w/50`"></vs-avatar>
+            <p class="name ml-1">
+              {{ cm.sender.nickname }}
             </p>
-          </li>
+            <p
+              v-if="cm.sender._id === owner"
+              class="owner ml-1"
+            >主人</p>
+            <p class="ml-2 text-xs text-gray-500">
+              {{ timeDiff(cm.created_at) }}
+            </p>
+          </div>
+          <p class="ml-8 text-sm text-gray-600">{{ cm.content }}</p>
+          <p
+            class="reply absolute cursor-pointer"
+            @click="showReplyInput(cm._id, cm.sender, 1)"
+          >回复</p>
+          <ul
+            v-if="cm.replies.length > 0"
+            class="ml-6 p-2 bg-gray-100 rounded-lg"
+          >
+            <li
+              v-for="(it, index) in cm.replies"
+              :key="index"
+              class="reply-item"
+            >
+              <p class="text-sm">
+                <span class="name cursor-pointer">
+                  {{ it.sender.nickname }}
+                  <span
+                    v-if="it.sender._id === owner"
+                    class="owner"
+                  >主人</span>
+                </span>
+                <span class="mx-1 text-gray-500">回复</span>
+                <span class="name cursor-pointer">
+                  {{ it.at.nickname }}
+                  <span
+                    v-if="it.sender._id === owner"
+                    class="owner"
+                  >主人</span>
+                  :
+                </span>
+                <span class="text-gray-500">
+                  {{ it.content }}
+                </span>
+                <i
+                  title="回复"
+                  class="reply-icon el-icon-chat-dot-square ml-2
+                 cursor-pointer text-base text-gray-600"
+                  @click="showReplyInput(cm._id, it.at, 2)"
+                ></i>
+              </p>
+            </li>
+            <div
+              v-if="currRep === cm._id"
+              class="mt-1 flex items-end"
+            >
+              <vs-input
+                class="flex-1 mr-1"
+                v-model="repContent"
+                :label-placeholder="placeholder"
+                @keyup.enter="replyComment(cm._id, it.sender._id)"
+              />
+              <vs-button
+                size="small"
+                @click="replyComment(cm._id, it.sender._id)"
+              >回复</vs-button>
+            </div>
+          </ul>
           <div
-            v-if="currRep === cm._id"
+            v-if="currMsg === cm._id"
             class="mt-1 flex items-end"
           >
             <vs-input
               class="flex-1 mr-1"
               v-model="repContent"
               :label-placeholder="placeholder"
-              @keyup.enter="replyComment(cm._id, it.sender._id)"
+              @keyup.enter="replyComment(cm._id, cm.sender._id)"
             />
             <vs-button
               size="small"
-              @click="replyComment(cm._id, it.sender._id)"
+              @click="replyComment(cm._id, cm.sender._id)"
             >回复</vs-button>
           </div>
-        </ul>
-        <div
-          v-if="currMsg === cm._id"
-          class="mt-1 flex items-end"
-        >
-          <vs-input
-            class="flex-1 mr-1"
-            v-model="repContent"
-            :label-placeholder="placeholder"
-            @keyup.enter="replyComment(cm._id, cm.sender._id)"
-          />
-          <vs-button
-            size="small"
-            @click="replyComment(cm._id, cm.sender._id)"
-          >回复</vs-button>
-        </div>
-      </li>
-    </ul>
+        </li>
+      </ul>
+    </VuePerfectScrollbar>
     <p
       v-else
       class="mt-6 p-2 text-center text-sm text-gray-500 bg-gray-100 rounded-lg"
     >
       还没有留言，快来抢沙发吧！
     </p>
+
+    <div
+      v-if="hideComment"
+      class="hide-comment absolute left-0 bottom-0 w-full text-sm text-center text-gray-600"
+    >
+      查看更多留言
+    </div>
   </div>
 </template>
 
 <script>
+import elementResizeDetectorMaker from 'element-resize-detector'
+import VuePerfectScrollbar from 'vue-perfect-scrollbar'
 import { timeDiff } from '@/utils/util'
 
 import { postComment, replyComment } from '@/request/api/goods'
 
 export default {
   name: 'DetailComment',
+  components: { VuePerfectScrollbar },
   props: {
     goodsId: String,
     comments: Array,
@@ -146,7 +167,19 @@ export default {
     repContent: '', // 回复内容
     currMsg: null, // 当前留言
     currRep: null, // 当前回复
+    hideComment: false, // 显示 “查看更多留言”
   }),
+
+  mounted() {
+    const erd = elementResizeDetectorMaker()
+    erd.listenTo(this.$refs.comment, (el) => {
+      if (el.offsetHeight >= 500) {
+        this.hideComment = true
+      } else {
+        this.hideComment = false
+      }
+    })
+  },
 
   methods: {
     async postComment() {
@@ -239,6 +272,25 @@ export default {
         display: inline-block;
       }
     }
+  }
+}
+
+.hide-comment {
+  height: 60px;
+  line-height: 80px;
+  background-size: 200%;
+  transition: background-position 0.5s linear;
+  cursor: pointer;
+  background-image: linear-gradient(
+    rgba(255, 255, 255, 0.1) 0%,
+    rgba(255, 255, 255, 1) 100%
+  );
+  &:hover {
+    background-position: 100% 0;
+    background-image: linear-gradient(
+      rgba(231, 238, 255, 0) 0%,
+      rgba(231, 238, 255, 1) 100%
+    );
   }
 }
 </style>
