800b7e6d26d17e04fea5199a136eb06a02b033b1
令狐少侠 <czc12580520@gmail.com>
2020-01-05 14:37:09 +0800

    ✅ 添加组件

---
+++ src/views/goods/GoodsCategory.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <GoodsList
+      :goodsList="goodsList"
+      :columns="4"
+    >
+    </GoodsList>
+  </div>
+</template>
+
+<script>
+import GoodsList from '@/views/goods/GoodsList.vue'
+
+export default {
+  name: 'GoodsCategory',
+  components: { GoodsList },
+
+  data: () => ({
+    goodsList: [],
+    category: 'all',
+    currentPage: 1,
+    pageSize: 10,
+    pagination: {},
+    searchText: '',
+  }),
+}
+</script>
