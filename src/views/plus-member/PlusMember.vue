5e8d937e3c124f80abb873b62a2d8d4e2484be48
czc <czc12580520@gmail.com>
2020-02-15 18:41:17 +0800

    ✅ 添加组件

---
+++ src/views/plus-member/PlusMember.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <el-image
+      class="mr-2 rounded-lg"
+      fit="cover"
+      src="https://gitee.com/chinesee/images/raw/master/img/img_024.png"
+    ></el-image>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PlusMember',
+}
+</script>
